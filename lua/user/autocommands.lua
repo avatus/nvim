03e7d8f778b62a1c1c6d750ae080255964c97ade
Michael Myers <michael.myers@goteleport.com>
2022-06-04 17:41:39 -0500

    removing dashboard header and fixing quickfix list

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -1,10 +1,11 @@
-vim.cmd [[
+vim.cmd([[
   augroup _general_settings
     autocmd!
     autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR> 
     autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200}) 
     autocmd BufWinEnter * :set formatoptions-=cro
     autocmd FileType qf set nobuflisted
+    autocmd FileType qf nnoremap <buffer> <CR> <CR>:cclose<CR>
   augroup end
   augroup _git
     autocmd!
@@ -24,7 +25,7 @@ vim.cmd [[
     autocmd!
     autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
   augroup end
-]]
+]])
 
 -- Autoformat
 -- augroup _lsp
